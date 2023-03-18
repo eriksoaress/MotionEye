933c65a355458275a127141006b94472905e5a20
fernandovs4 <fervsantos3000@gmail.com>
2023-03-18 13:29:42 -0300

    gravacao de video

--- main.py
+++ main.py
@@ -13,16 +13,6 @@ def criar_indices(min_i, max_i, min_j, max_j):
     idx_j = np.array([e[1] for e in L])
     idx = np.vstack((idx_i, idx_j))
     return idx
-
-def run():
-   def criar_indices(min_i, max_i, min_j, max_j):
-    import itertools
-    L = list(itertools.product(range(min_i, max_i), range(min_j, max_j)))
-    idx_i = np.array([e[0] for e in L])
-    idx_j = np.array([e[1] for e in L])
-    idx = np.vstack((idx_i, idx_j))
-    return idx
-   
 def run():
     cap = cv.VideoCapture(0)
     width = 320
